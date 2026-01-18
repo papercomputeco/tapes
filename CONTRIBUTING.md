1cb02d781d8979dc9aa0be41ea1b4b8d1ef97f7e
John McBride <jpmmcbride@gmail.com>
2026-01-18 15:28:38 -0500

    feat: Nix dev environment
    Signed-off-by: John McBride <jpmmcbride@gmail.com>

---
+++ CONTRIBUTING.md
@@ -0,0 +1,8 @@
+# Contributing
+
+## Nix flake and `direnv`
+
+We encourage contributors to utilize the Nix flake which carries all development dependencies
+and has automatic `direnv` support:
+to enter the nix development shell, run `nix develop`.
+Allow automatic environment loading with `direnv` via `direnv allow`.
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Tapes - Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Go toolchain
+            go_1_25
+
+            # Build tools
+            gnumake
+
+            # Required for go-sqlite3 (cgo)
+            gcc
+            pkg-config
+          ];
+
+          # Enable cgo for sqlite3 support
+          CGO_ENABLED = "1";
+
+          shellHook = ''
+            echo "Tapes development environment"
+            echo ""
+            echo "Go version: $(go version)"
+            echo ""
+            echo "Available make targets:"
+            make help
+          '';
+        };
+      }
+    );
+}
